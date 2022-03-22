2f41aeb1fd2b7aec0476405e17d9c548e8c55d4b
Yifeng CHENG <19251076@life.hkbu.edu.hk>
2022-03-22 13:52:22 +0800

    Adding AR, rotating box and emoji

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -16,6 +16,8 @@ import {
     ViroNode,
     ViroSpotLight,
     ViroAnimations,
+    ViroARTrackingTargets,
+    ViroARImageMarker
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -33,28 +35,42 @@ export default class HelloWorldSceneAR extends Component {
     }
 
     render() {
+
+        const anchorFound = () => {
+            console.log("Anchor Image Detected")
+        }
+
+        ViroARTrackingTargets.createTargets({
+            gridImage: {
+                source: require('./res/360_park.jpg'),
+                orientation: 'up',
+                physicalWidth: 0.1
+            }
+        })
+
         return (
             <ViroARScene onTrackingUpdated={this._onInitialized}>
                 <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]}
                           style={styles.helloWorldTextStyle}/>
                 <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]}
                          animation={{name: "rotate", run: true, loop: true}}/>
-                <ViroAmbientLight color={"#aaaaaa"}/>
-                <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
-                               position={[0, 3, 1]} color="#ffffff" castsShadow={true}/>
+                <ViroARImageMarker target="gridImage" onAnchorFound={anchorFound}>
+                    <ViroAmbientLight color={"#aaaaaa"}/>
+                    <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
+                                   position={[0, 3, 1]} color="#ffffff" castsShadow={true}/>
 
-                <ViroNode position={[0, -1, 0]} dragType="FixedToWorld" onDrag={() => {
-                }}>
+
                     <Viro3DObject
                         source={require('./res/emoji_smile/emoji_smile.vrx')}
                         resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
                             require('./res/emoji_smile/emoji_smile_normal.png'),
                             require('./res/emoji_smile/emoji_smile_specular.png')]}
-                        position={[0, .5, 0]}
-                        scale={[.2, .2, .2]}
+                        position={[0, 0, 0]}
+                        scale={[.1, .1, .1]}
+                        rotation={[-90.,0,0.]}
                         type="VRX"/>
-                </ViroNode>
 
+                </ViroARImageMarker>
             </ViroARScene>
         );
     }
@@ -96,4 +112,6 @@ ViroAnimations.registerAnimations({
         duration: 250, //.25 seconds
     },
 });
+
+
 module.exports = HelloWorldSceneAR;
